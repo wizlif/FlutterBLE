90dd63e17cd27838c113eca465aaa4dae75cf9dd
Isaac Obella <wizlif.144@gmail.com>
2022-10-24 19:49:19 +0300

    Add characteristic check
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

--- lib/pages/device_screen/parts/bluetooth_service_list.dart
+++ lib/pages/device_screen/parts/bluetooth_service_list.dart
@@ -1,14 +1,21 @@
 part of '../device_screen.dart';
 
-class BluetoothServiceList extends StatelessWidget {
+class BluetoothServiceList extends StatefulWidget {
   final BluetoothDevice device;
   const BluetoothServiceList({Key? key, required this.device})
       : super(key: key);
 
   @override
+  State<BluetoothServiceList> createState() => _BluetoothServiceListState();
+}
+
+class _BluetoothServiceListState extends State<BluetoothServiceList> {
+  bool _isNotifying = false;
+
+  @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<BluetoothService>>(
-      stream: device.services,
+      stream: widget.device.services,
       initialData: const [],
       builder: (c, snapshot) {
         final services = snapshot.data ?? [];
@@ -32,6 +39,12 @@ class BluetoothServiceList extends StatelessWidget {
                 log("SERVICE_UUID: ${c.serviceUuid.toByteArray()}");
                 log("CHARACTERISTIC UUID: ${c.uuid.toByteArray()}");
                 log("NOTIFYING: ${c.isNotifying}");
+
+                // Set notifying to enabled
+                // when c.isNotifying && is characteristic 49
+                if (c.isNotifying && c.isCharacteristic49) {
+                  _isNotifying = true;
+                }
 
                 return CharacteristicTile(
                   characteristic: c,
@@ -65,12 +78,10 @@ class BluetoothServiceList extends StatelessWidget {
         .toList();
   }
 
+  /// Write chunks if is characteristic 42
+  /// and [_isNotifying] is true
   Future<void> _onWritePressed(BluetoothCharacteristic c) async {
-    final isNotifyingXtic42 = c.isNotifyingCharacteristic42;
-
-    log("NOTIFYING CHARACTERISTIC 42: $isNotifyingXtic42");
-
-    if (isNotifyingXtic42) {
+    if (c.isCharacteristic42 && _isNotifying) {
       final chunks = await Utils.localPath();
 
       await Future.forEach(chunks, (chunk) async {
@@ -83,8 +94,26 @@ class BluetoothServiceList extends StatelessWidget {
 }
 
 extension CharacteristicX on BluetoothCharacteristic {
-  bool get isNotifyingCharacteristic42 =>
-      isNotifying && uuid.toString() == characteristic42Uuid;
+  bool get isCharacteristic42 => uuid.toString() == characteristic42Uuid;
+  bool get isCharacteristic49 => uuid.toByteArray() == characteristic49Uuid;
 }
 
 const characteristic42Uuid = "0000fe42-8e22-4541-9d4c-21edae82ed19";
+const characteristic49Uuid = [
+  0,
+  0,
+  254,
+  66,
+  142,
+  34,
+  69,
+  65,
+  157,
+  76,
+  33,
+  237,
+  174,
+  130,
+  237,
+  25
+];
